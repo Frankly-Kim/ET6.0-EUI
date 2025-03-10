d300c3452c970b6129517d1adf018a1f5781e604
Frankly_Kim <51369459@qq.com>
2025-03-10 21:06:33 +0800

    ET6.0-EUI    第18课提交

--- Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
+++ Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
@@ -3,8 +3,8 @@ using System.Text.RegularExpressions;
 
 namespace ET
 {
-    [FriendClass(typeof(Account))]
-    public class C2A_LoginAccountHandler : AMRpcHandler<C2A_LoginAccount, A2C_LoginAccount>
+    [FriendClass(typeof (Account))]
+    public class C2A_LoginAccountHandler: AMRpcHandler<C2A_LoginAccount, A2C_LoginAccount>
     {
         protected override async ETTask Run(Session session, C2A_LoginAccount request, A2C_LoginAccount response, Action reply)
         {
@@ -17,81 +17,89 @@ namespace ET
 
             session.RemoveComponent<SessionAcceptTimeoutComponent>();
 
+            if (session.GetComponent<SessionLockingComponent>() != null)
+            {
+                response.Error = ErrorCode.ERR_RequestRepeatedly;
+                reply();
+                session.Disconnect().Coroutine();
+                return;
+            }
+
             if (string.IsNullOrEmpty(request.AccountName) || string.IsNullOrEmpty(request.Password))
             {
-                response.Error = ErrorCode.Err_LoginInfoError;
+                response.Error = ErrorCode.ERR_LoginInfoError;
                 reply();
-                session.Dispose();
+                session.Disconnect().Coroutine();
                 return;
             }
 
             if (!Regex.IsMatch(request.AccountName.Trim(), @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,15}$"))
             {
-                response.Error = ErrorCode.Err_LoginInfoError;
+                response.Error = ErrorCode.ERR_LoginInfoError;
                 reply();
-                session.Dispose();
+                session.Disconnect().Coroutine();
                 return;
             }
 
             if (!Regex.IsMatch(request.Password.Trim(), @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,15}$"))
             {
-                response.Error = ErrorCode.Err_LoginInfoError;
+                response.Error = ErrorCode.ERR_LoginInfoError;
                 reply();
-                session.Dispose();
+                session.Disconnect().Coroutine();
                 return;
             }
 
-            var accountInfoList = await DBManagerComponent.Instance.GetZoneDB((session.DomainZone()))
-                    .Query<Account>(d => d.AccountName.Equals(request.AccountName.Trim()));
-            Account account = null;
-
-            if (accountInfoList.Count > 0)
+            using (session.AddComponent<SessionLockingComponent>())
             {
-                account = accountInfoList[0];
-                session.AddChild(account);
-                if (account.AccountType == (int)AccountType.BlackList)
+                using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.LoginAccount, request.AccountName.Trim().GetHashCode()))
                 {
-                    response.Error = ErrorCode.Err_LoginInfoError;
+                    var accountInfoList = await DBManagerComponent.Instance.GetZoneDB((session.DomainZone()))
+                            .Query<Account>(d => d.AccountName.Equals(request.AccountName.Trim()));
+                    Account account = null;
+
+                    if (accountInfoList != null && accountInfoList.Count > 0)
+                    {
+                        account = accountInfoList[0];
+                        session.AddChild(account);
+                        if (account.AccountType == (int)AccountType.BlackList)
+                        {
+                            response.Error = ErrorCode.ERR_LoginInfoError;
+                            reply();
+                            session.Disconnect().Coroutine();
+                            account?.Dispose();
+                            return;
+                        }
+
+                        if (account.Password.Equals(request.Password))
+                        {
+                            response.Error = ErrorCode.ERR_LoginInfoError;
+                            reply();
+                            session.Disconnect().Coroutine();
+                            account?.Dispose();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        account = session.AddChild<Account>();
+                        account.AccountName = request.AccountName.Trim();
+                        account.Password = request.Password.Trim();
+                        account.CreateTime = TimeHelper.ServerNow();
+                        account.AccountType = (int)AccountType.General;
+                        await DBManagerComponent.Instance.GetZoneDB(session.DomainZone()).Save(account);
+                    }
+
+                    string Token = TimeHelper.ServerNow().ToString() + RandomHelper.RandomNumber(int.MinValue, int.MaxValue).ToString();
+                    session.DomainScene().GetComponent<TokenComponent>().Remove(account.Id);
+                    session.DomainScene().GetComponent<TokenComponent>().Add(account.Id, Token);
+
+                    response.AccountId = account.Id;
+                    response.Token = Token;
+
                     reply();
-                    session.Dispose();
-                    return;
+                    account?.Dispose();
                 }
-
-                if (account.Password.Equals(request.Password))
-                {
-                    response.Error = ErrorCode.Err_LoginInfoError;
-                    reply();
-                    session.Dispose();
-                    
-                    return;
-                }
-
-
-
             }
-            else
-            {
-                account = session.AddChild<Account>();
-                account.AccountName = request.AccountName.Trim();
-                account.Password = request.Password.Trim();
-                account.CreateTime = TimeHelper.ServerNow();
-                account.AccountType = (int)AccountType.General;
-                await DBManagerComponent.Instance.GetZoneDB(session.DomainZone()).Save(account);
-                
-
-            }
-
-            string Token = TimeHelper.ServerNow().ToString() + RandomHelper.RandomNumber(int.MinValue, int.MaxValue).ToString();
-            session.DomainScene().GetComponent<TokenComponent>().Remove(account.Id);
-            session.DomainScene().GetComponent<TokenComponent>().Add(account.Id, Token);
-
-            response.AccountId = account.Id;
-            response.Token = Token;
-
-            reply();
-
-
-            
         }
     }
 }
--- Unity/Codes/Model/Module/Message/ErrorCode.cs
+++ Unity/Codes/Model/Module/Message/ErrorCode.cs
@@ -7,15 +7,19 @@ namespace ET
         // 1-11004 是SocketError请看SocketError定义
         //-----------------------------------
         // 100000-109999是Core层的错误
-        
+
         // 110000以下的错误请看ErrorCore.cs
-        
+
         // 这里配置逻辑层的错误码
         // 110000 - 200000是抛异常的错误
         // 200001以上不抛异常
 
         public const int ERR_NetWorkError = 200002; //网络错误
-        public const int Err_LoginInfoError = 200003; //登录信息错误
-
+        public const int ERR_LoginInfoError = 200003; //登录信息错误
+        public const int ERR_AccountNameFormError = 200004; //登录账号格式错误
+        public const int ERR_PasswordFormError = 200005; //登录密码格式错误
+        public const int ERR_AccountInBlackListError = 200006; //账号处于黑名单中
+        public const int ERR_LoginPasswordError = 200007; //登录密码错误
+        public const int ERR_RequestRepeatedly = 200008; //多次请求
     }
 }
